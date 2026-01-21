7127cf52e996ecbc5a3a8c1d273a0c192ce6ab4f
SeaJay <mcatania164@gmail.com>
2026-01-21 12:56:55 -0500

    Update append_update_issues.sql

--- DBFiles/append_update_issues.sql
+++ DBFiles/append_update_issues.sql
@@ -77,7 +77,7 @@ VALUES
     (419,6,'zStandard','2022-10-25',3,3,'2026-01-19'), --beyond white night 6
     (419,7,'zStandard','2022-12-27',3,3,'2026-01-19'), --beyond white night 7
     (419,8,'zStandard','2023-02-14',3,3,'2026-01-20'), --beyond white night 8
-    (518,1,'zStandard','2023-05-09',3,3,''), --white night GJ 1
+    (518,1,'zStandard','2023-05-09',3,3,'2026-01-20'), --white night GJ 1
     (518,2,'zStandard','2023-06-20',3,3,''), --white night GJ 2
     (518,3,'zStandard','2023-07-18',3,3,''), --white night GJ 3
     (518,4,'zStandard','2023-08-15',3,3,''), --white night GJ 4
@@ -95,3 +95,4 @@ WHERE issue_id = ; --absolute batman 15
 UPDATE issues
 SET read_date = '2026-01-16'
 WHERE issue_id = ; --absolute flash 10
+
